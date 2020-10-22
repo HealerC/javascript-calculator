9655fa56bbf3202723050ecea94d06180997838b
Uyioghosa Nosa-Ihaza <ihazauyi@gmail.com>
2020-10-22 11:06:57 +0100

    Remove the build from .gitignore

---
+++ build/precache-manifest.ee3c17e1d02ef536cd8d3c9f2bcb3c1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "244086563d86a4b42319e7888ee45a21",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4d75729569349ad98c4e",
+    "url": "./static/css/main.0bdf95c2.chunk.css"
+  },
+  {
+    "revision": "d74ce96d8ffae0bb77f4",
+    "url": "./static/js/2.6a941fd1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.6a941fd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d75729569349ad98c4e",
+    "url": "./static/js/main.1f504d17.chunk.js"
+  },
+  {
+    "revision": "3a370c523d838257c630",
+    "url": "./static/js/runtime-main.fd4cb08d.js"
+  }
+]);
